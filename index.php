6126fdb602a21485ee813bb2c26b2a587cdab4c6
Malyshev Andrei <Agentus2005@yandex.ru>
2016-04-26 23:10:01 +0300

    add phpdoc

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
+/**
+ * Class World
+ */
 class World
 {
+    /**
+     * @author My Name <my.name@example.com>
+     * @return string
+     */
     public function hello()
     {
         return 'Hello, World!';
