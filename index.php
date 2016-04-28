b9452c49091a5a4509cd7251857c5c9c397a4aa1
777Vasya77 <777Vasya77>
2016-04-28 15:25:49 +0300

    delete class

--- index.php
+++ index.php
@@ -1,19 +1,5 @@
 <?php
 
-/**
- * Class World
- */
-class World
-{
-    /**
-     * @author My Name <my.name@example.com>
-     * @return string
-     */
-    public function hello()
-    {
-        return 'Hello, World!';
-    }
-}
 // Hello
 
 //hello 13579333
